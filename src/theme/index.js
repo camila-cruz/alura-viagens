30d6b42f0f1cc1d98035f7d849fb6c51387cddd3
Camila <cruz.camila001@gmail.com>
2021-03-06 20:41:02 -0300

    refactor(theme): add font-family to the typography variants

--- src/theme/index.js
+++ src/theme/index.js
@@ -5,43 +5,52 @@ const colors = {
 };
 
 const fontFamily = {
-  title: '\'Pattaya\'',
-  text: '\'Roboto\'',
+  title: '\'Pattaya\', cursive',
+  text: '\'Roboto\', sans-serif',
 };
 
 const typographyVariants = {
-  header: {
-    fontSize: {
-      xs: '36px',
-      md: '72px',
-    },
+  headerXS: {
+    fontSize: '36px',
     fontWeight: '400',
     lineHeight: 1.25,
+    fontFamily: fontFamily.title,
+  },
+  headerMD: {
+    fontSize: '72px',
+    fontWeight: '400',
+    lineHeight: 1.25,
+    fontFamily: fontFamily.text,
   },
   subheader: {
     fontSize: '24px',
     fontWeight: '400',
     lineHeight: 1.25,
+    fontFamily: fontFamily.text,
   },
   label: {
     fontSize: '18px',
     fontWeight: '400',
     lineHeight: 1.25,
+    fontFamily: fontFamily.text,
   },
   inputText: {
     fontSize: '18px',
     fontWeight: '400',
     lineHeight: 1.25,
+    fontFamily: fontFamily.text,
   },
   radioText: {
     fontSize: '14px',
     fontWeight: '400',
     lineHeight: 1,
+    fontFamily: fontFamily.text,
   },
   buttonText: {
     fontSize: '18px',
     fontWeight: '700',
     lineHeight: 1,
+    fontFamily: fontFamily.text,
   },
 };
 
