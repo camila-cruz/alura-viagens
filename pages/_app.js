6d8bac79c702a444d0784a945afbc9fdeaa709b1
Camila <cruz.camila001@gmail.com>
2021-03-06 20:17:18 -0300

    feat(global): add a theme to the project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Head from 'next/head';
+import theme from '../src/theme';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -9,16 +11,15 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-};
-
 // eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Alura Viagens</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Pattaya&display=swap" rel="stylesheet" />
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         {/* eslint-disable-next-line react/jsx-props-no-spreading */}
---
+++ src/theme/index.js
@@ -0,0 +1,52 @@
+const colors = {
+  background: '#FFFFFF',
+  primary: '#35B6FF',
+  secondary: '#000000',
+};
+
+const fontFamily = {
+  title: '\'Pattaya\'',
+  text: '\'Roboto\'',
+};
+
+const typographyVariants = {
+  header: {
+    fontSize: {
+      xs: '36px',
+      md: '72px',
+    },
+    fontWeight: '400',
+    lineHeight: 1.25,
+  },
+  subheader: {
+    fontSize: '24px',
+    fontWeight: '400',
+    lineHeight: 1.25,
+  },
+  label: {
+    fontSize: '18px',
+    fontWeight: '400',
+    lineHeight: 1.25,
+  },
+  inputText: {
+    fontSize: '18px',
+    fontWeight: '400',
+    lineHeight: 1.25,
+  },
+  radioText: {
+    fontSize: '14px',
+    fontWeight: '400',
+    lineHeight: 1,
+  },
+  buttonText: {
+    fontSize: '18px',
+    fontWeight: '700',
+    lineHeight: 1,
+  },
+};
+
+export default {
+  colors,
+  fontFamily,
+  typographyVariants,
+};
