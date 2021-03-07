7a6fa280e8ecf020e858252c18fced55956ba2e2
Camila <cruz.camila001@gmail.com>
2021-03-07 16:31:32 -0300

    feat(foundation): add box component to wrap the main content

---
+++ src/components/foundation/layout/Box/index.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import { propToStyle } from '../../../../theme/utils/propToStyle';
+
+export const Box = styled.div`
+  background-color: ${({ theme }) => theme.colors.background};
+  box-shadow: 0px 0px 20px 5px rgba(0, 0, 0, 0.3);
+
+  ${propToStyle('display')}
+  ${propToStyle('flexDirection')}
+  ${propToStyle('alignItems')}
+  ${propToStyle('justifyContent')}
+  ${propToStyle('margin')}
+  ${propToStyle('width')}
+  ${propToStyle('maxWidth')}
+  ${propToStyle('background')}
+`;
